693fc52cf49c0acce4c5c231e55149d547114821
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-01-17 11:38:09 +0100

    Support for zigate usb and wifi

--- zigpy_zigate/api.py
+++ zigpy_zigate/api.py
@@ -9,7 +9,17 @@ class ZiGate:
         self._zigate = None
 
     async def connect(self, device, baudrate=115200):
-        self._zigate = zigate.ZiGate(device, auto_start=False)
+        if '.' in device:  # supposed I.P:PORT
+            host_port = device.split(':', 1)
+            host = host_port[0]
+            port = None
+            if len(host_port) == 2:
+                port = int(host_port[1])
+            LOGGER.info('Configuring ZiGate WiFi {} {}'.format(host, port))
+            self._zigate = zigate.ZiGateWiFi(host, port, auto_start=False)
+        else:
+            LOGGER.info('Configuring ZiGate USB {}'.format(device))
+            self._zigate = zigate.ZiGate(device, auto_start=False)
 
     def __getattr__(self, name):
         return self._zigate.__getattribute__(name)
