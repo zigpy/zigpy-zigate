5ce1760d62d006d16d0cf0231b91a0e8570bb4f8
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-03-12 10:55:06 +0100

    raw aps data req

--- zigpy_zigate/zigbee/application.py
+++ zigpy_zigate/zigbee/application.py
@@ -125,7 +125,7 @@ class ControllerApplication(zigpy.application.ControllerApplication):
             reply_fut = asyncio.Future()
         self._pending[sequence] = (send_fut, reply_fut)
 
-        v = self._zigate.raw_aps_data_request('{:04x}'.format(nwk), src_ep, dst_ep, profile, cluster, data, security=0)
+        v = self._zigate.raw_aps_data_request('{:04x}'.format(nwk), src_ep, dst_ep, profile, cluster, data)
         self._zigate_seq[sequence] = v.sequence
 
         if v.status != 0:
