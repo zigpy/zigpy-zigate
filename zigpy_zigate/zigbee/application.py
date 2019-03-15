dcecb5747383e40f1a41a8d201ba6cd40ae30480
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-03-15 12:37:40 +0100

    Fix raw_aps_data_request

--- zigpy_zigate/zigbee/application.py
+++ zigpy_zigate/zigbee/application.py
@@ -118,15 +118,14 @@ class ControllerApplication(zigpy.application.ControllerApplication):
 
     @zigpy.util.retryable_request
     async def request(self, nwk, profile, cluster, src_ep, dst_ep, sequence, data, expect_reply=True, timeout=10):
-        LOGGER.debug('request %s',(nwk, profile, cluster, src_ep, dst_ep, sequence, data, expect_reply, timeout))
+        LOGGER.debug('request %s', (nwk, profile, cluster, src_ep, dst_ep, sequence, data, expect_reply, timeout))
         assert sequence not in self._pending
         send_fut = asyncio.Future()
         reply_fut = None
         if expect_reply:
             reply_fut = asyncio.Future()
         self._pending[sequence] = (send_fut, reply_fut)
-        src_ep = 1
-        v = self._zigate.raw_aps_data_request('{:04x}'.format(nwk), src_ep, dst_ep, profile, cluster, data[1:])
+        v = self._zigate.raw_aps_data_request('{:04x}'.format(nwk), src_ep, dst_ep, profile, cluster, data)
         self._zigate_seq[sequence] = v.sequence
 
         if v.status != 0:
