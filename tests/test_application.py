40365269bc4612cb40600a9c5d84126679108239
Sébastien RAMAGE <sebastien.ramage@gmail.com>
2019-11-26 11:18:54 +0100

    Fix test

--- tests/test_application.py
+++ tests/test_application.py
@@ -14,10 +14,12 @@ def app():
 
 
 def test_zigpy_ieee(app):
+    cluster = mock.MagicMock()
+    cluster.cluster_id = 0x0000
     data = b'\x01\x02\x03\x04\x05\x06\x07\x08'
 
     zigate_ieee, _ = t.EUI64.deserialize(data)
     app._ieee = zigpy_types.EUI64(zigate_ieee)
 
-    dst_addr = app.get_dst_address(0x0000)
+    dst_addr = app.get_dst_address(cluster)
     assert dst_addr.serialize() == b'\x03' + data[::-1] + b'\x01'
