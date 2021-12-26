69cc2a91caed82a4fb8d9b512f2aaa3b6040778e
mbadaire@gmail.com <mbadaire@gmail.com>
2021-12-26 15:45:46 +0100

    add version_int to get a value to compare values

--- zigpy_zigate/api.py
+++ zigpy_zigate/api.py
@@ -335,6 +335,10 @@ class ZiGate:
         version = '{}.{}'.format(version[0], version[1:])
         return version
 
+    async def version_int(self):
+        version, lqi = await self.version()
+        return version[1]
+
     async def get_network_state(self):
         return await self.command(0x0009, wait_response=0x8009)
 
