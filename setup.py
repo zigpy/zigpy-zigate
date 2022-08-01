64972be410e0968eb36d6136ec49c19bcc62bd59
puddly <32534428+puddly@users.noreply.github.com>
2022-08-01 19:11:05 -0400

    Remove checks for Raspberry Pi

--- setup.py
+++ setup.py
@@ -1,80 +1,29 @@
 """Setup module for zigpy-zigate"""
-import os
+
+import pathlib
 
 from setuptools import find_packages, setup
 from zigpy_zigate import __version__
-
-
-# extracted from https://raspberrypi.stackexchange.com/questions/5100/detect-that-a-python-program-is-running-on-the-pi
-def is_raspberry_pi(raise_on_errors=False):
-    """Checks if Raspberry PI.
-
-    :return:
-    """
-    try:
-        with open('/proc/cpuinfo', 'r') as cpuinfo:
-            found = False
-            for line in cpuinfo:
-                if line.startswith('Hardware'):
-                    found = True
-                    label, value = line.strip().split(':', 1)
-                    value = value.strip()
-                    if value not in (
-                        'BCM2708',
-                        'BCM2709',
-                        'BCM2835',
-                        'BCM2836'
-                    ):
-                        if raise_on_errors:
-                            raise ValueError(
-                                'This system does not appear to be a '
-                                'Raspberry Pi.'
-                            )
-                        else:
-                            return False
-            if not found:
-                if raise_on_errors:
-                    raise ValueError(
-                        'Unable to determine if this system is a Raspberry Pi.'
-                    )
-                else:
-                    return False
-    except IOError:
-        if raise_on_errors:
-            raise ValueError('Unable to open `/proc/cpuinfo`.')
-        else:
-            return False
-
-    return True
-
-
-requires = [
-    'pyserial>=3.5',
-    'pyserial-asyncio>=0.5; platform_system!="Windows"',
-    'pyserial-asyncio!=0.5; platform_system=="Windows"',  # 0.5 broke writesv
-    'pyusb>=1.1.0',
-    'zigpy>=0.47.0',
-]
-
-if is_raspberry_pi():
-    requires.append('RPi.GPIO')
-
-this_directory = os.path.join(os.path.abspath(os.path.dirname(__file__)))
-with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
 
 setup(
     name="zigpy-zigate",
     version=__version__,
     description="A library which communicates with ZiGate radios for zigpy",
-    long_description=long_description,
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
     url="http://github.com/zigpy/zigpy-zigate",
     author="Sébastien RAMAGE",
     author_email="sebatien.ramage@gmail.com",
     license="GPL-3.0",
     packages=find_packages(exclude=['tests']),
-    install_requires=requires,
+    install_requires=[
+        'pyserial>=3.5',
+        'pyserial-asyncio>=0.5; platform_system!="Windows"',
+        'pyserial-asyncio!=0.5; platform_system=="Windows"',  # 0.5 broke writes
+        'pyusb>=1.1.0',
+        'zigpy>=0.47.0',
+        'gpiozero',
+    ],
     tests_require=[
         'pytest',
         'pytest-asyncio',
